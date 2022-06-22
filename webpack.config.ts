4261e570537a09f201fe8b6a4531a06dde9950ff
haru <haruki6180@gmail.com>
2022-06-22 23:23:30 +0900

    webpack.config.json　修正

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,11 +3,23 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "src/index.tsx",
+  entry: {
+    index: path.join(__dirname, 'src', 'index.tsx')
+  },
   output: {
     path: path.resolve(__dirname, "build"),
+    filename: 'main.ts'
   },
   module: {
-    rules: [{ test: /\.ts|tsx$/, use: "ts-loader" }],
+    rules: [{ test: /\.ts|tsx$/, use: "ts-loader" },
+    {
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader',
+      options: {},
+  }],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts',".js"],
+    modules: ["node_modules",path.resolve(__dirname, "app")],
   },
 };
