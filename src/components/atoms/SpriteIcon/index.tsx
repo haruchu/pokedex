f968adb1fd7e73c7b5286a1859dc499ac47a917f
haru <haruki6180@gmail.com>
2022-06-26 17:48:01 +0900

    コメント追加

--- src/components/atoms/SpriteIcon/index.tsx
+++ src/components/atoms/SpriteIcon/index.tsx
@@ -3,7 +3,9 @@ import { IconWrapper } from "./style";
 import { TYPE_COLOR } from "../../../styles/color";
 
 type SpriteIconProps = {
+  // firebase strageへの画像パスをPropsで渡す
   SpriteImgPath: string;
+  // 英語変換されたタイプ
   type: keyof typeof TYPE_COLOR | "ERROR";
 };
 
