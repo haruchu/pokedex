8af27d02c2d6f47a35432339450803c31dc72037
haru <haruki6180@gmail.com>
2022-06-26 16:34:18 +0900

    firebase storage設定

---
+++ src/FirebaseConfig.ts
@@ -0,0 +1,16 @@
+import { getStorage } from "@firebase/storage"
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBLdSyf2SvJjiO5a7hcv6eEU8eH-h3Ofk",
+  authDomain: "pokedex-haru.firebaseapp.com",
+  projectId: "pokedex-haru",
+  storageBucket: "pokedex-haru.appspot.com",
+  messagingSenderId: "866441621052",
+  appId: "1:866441621052:web:c805b837d80cea6fa09d39",
+  measurementId: "G-NNERWZYLV7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage();
