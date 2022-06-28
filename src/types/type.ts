7f76daf1e0d24ce8a83ccbd1dd80ac0eb742ae3a
haru <haruki6180@gmail.com>
2022-06-28 16:19:41 +0900

    statusの型定義,　status項目を日本語化する関数作成

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,3 +1,12 @@
 import { TYPE_COLOR } from "../styles/color";
 
 export type EnglishType = keyof typeof TYPE_COLOR | "ERROR";
+
+export type StatusType = {
+  hp: number;
+  attack: number;
+  defence: number;
+  spAttack: number;
+  spDefence: number;
+  speed: number;
+};
